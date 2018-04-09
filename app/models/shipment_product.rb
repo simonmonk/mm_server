f96cb24cf88a82722fe86364b31f957bd5d203e0
Simon Monk <evilgeniusauthor@gmail.com>
2018-04-09 19:04:57 +0100

    added currency to invoice

--- app/models/shipment_product.rb
+++ app/models/shipment_product.rb
@@ -1,4 +1,17 @@
 class ShipmentProduct < ApplicationRecord
   belongs_to :shipment
   belongs_to :product
+    
+    
+  def wholesale_in_currency()
+     if (shipment.retailer.pref_currency == 'USD')
+         if (product.wholesale_price_usd)
+             return product.wholesale_price_usd
+         else
+             return 0
+         end
+     end
+     return product.wholesale_price
+  end
+    
 end
