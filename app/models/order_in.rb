5ad958164a482382ab2dc16401d05fee49b01768
Simon Monk <evilgeniusauthor@gmail.com>
2018-04-20 15:12:11 +0100

    fixed order_ins bug

--- app/models/order_in.rb
+++ app/models/order_in.rb
@@ -36,8 +36,12 @@ class OrderIn < ApplicationRecord
           return "nothing"
       end
       first_item = self.order_in_lines[0]
+      first_name = "missing part"
+      if (first_item.part)
+          first_name = first_item.part.name
+      end
       if (num_items == 1)
-         return first_item.qty.to_s + " x " + first_item.part.name  
+         return first_item.qty.to_s + " x " + first_name  
       end
       if (num_items == 2)
           return first_item.part.name + " and one other item."
