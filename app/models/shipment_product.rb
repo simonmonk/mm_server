3926c668fec490264fb86e1f512bf6da2914ed3a
Simon Monk <evilgeniusauthor@gmail.com>
2018-05-23 21:37:26 +0100

    added dollar retail price to shipment

--- app/models/shipment_product.rb
+++ app/models/shipment_product.rb
@@ -4,6 +4,13 @@ class ShipmentProduct < ApplicationRecord
     
     
   def wholesale_in_currency()
+     if (shipment.retailer.name == 'amazon.com')
+         if (product.retail_price_usd)
+             return product.retail_price_usd
+         else
+             return 0
+         end
+     end
      if (shipment.retailer.pref_currency == 'USD')
          if (product.wholesale_price_usd)
              return product.wholesale_price_usd
